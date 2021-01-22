93a29c0b2f96c3d6ae9c9b6def6f946e4fe41a1a
Cederlund1999 <cederlund1999@gmail.com>
2021-01-22 14:57:48 +0100

    Test för att kunna skapa inlägg med hjälp av knapp

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 var posts =1;
 function buttonClick(){
     document.getElementById('input-number').value = posts++;
+    
+    var parent = document.querySelector("main");
+    parent.className ="main1";
+
+            var child = document.createElement("section");
+            child.className = "inlägg";
+
+            
+            var title = document.createElement("h3");
+            var blurb = document.createElement("p");
+
+            title.innerHTML = "Title";
+            blurb.innerHTML ="hej";
+
+    
+    
+
+            child.appendChild(title);
+            child.appendChild(blurb);
+            parent.appendChild(child);
+
 }
 
