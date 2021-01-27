330d5e80d2dc10f722a62bbe58b59d8506f51f4d
Cederlund1999 <cederlund1999@gmail.com>
2021-01-27 16:47:07 +0100

    går att edita och ta bort alla sections

--- main.js
+++ main.js
@@ -1,26 +1,57 @@
+window.onload = function() {
+    //RemoveOneSection();
+}
+
+
 let posts =1;
 function buttonClick(){
     document.getElementById('input-number').value = posts++;
-    
     let parent = document.querySelector("main");
     parent.className ="main1";
+
+            
 
             let child = document.createElement("section");
             child.className = "inlägg";
 
-            
             let title = document.createElement("h3");
-            let blurb = document.createElement("p");
+            title.contentEditable = true;
+
+            let textbox = document.createElement("p");
+            textbox.contentEditable =true;
+
+            
+            
+        
 
             title.innerHTML = "Title";
-            blurb.innerHTML ="hej";
+            textbox.innerHTML ="hej";
+            
+        
+            
 
-    
-    
-
+            
             child.appendChild(title);
-            child.appendChild(blurb);
+            child.appendChild(textbox);
             parent.appendChild(child);
 
+            
+            console.log(i);
+
+            
+            
 }
+function RemoveOneSection(){
+    let sections = document.querySelectorAll("section");
+    for(let i =0; i < sections.length; i++){
+        sections[i].remove();
+       // document.querySelector("input").();
+        
+        
+    }
+    
+    
+
+}
+
 
